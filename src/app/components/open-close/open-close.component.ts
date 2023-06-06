536f5904820c109c7a7e28f805aa7a9fb3637233
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-06 12:03:41 +0200

    update fix (ho dimenticato import{browseranimationmodule}
    update fix (ho dimenticato import{browseranimationmodule}

--- src/app/components/open-close/open-close.component.ts
+++ src/app/components/open-close/open-close.component.ts
@@ -11,19 +11,15 @@ import { Component } from '@angular/core';
         height: '200px',
         opacity: 1,
         backgroundColor: 'yellow'
-      })
-      ),
+      })),
       state('closed', style({
         height: '100px',
         opacity: 0.8,
         backgroundColor: 'blue'
-      })
-      ),
+      })),
       transition('open => closed', [animate('1s')]),
       transition('closed => open', [animate('0.5s')]),
-    ],
-    ),
-  ],
+    ])]
 })
 export class OpenCloseComponent {
 
