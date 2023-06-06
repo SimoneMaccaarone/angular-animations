1ac6ca8a613b99650ed8e1c5cac40437695f1d69
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-06 11:54:13 +0200

    add TRIGGER
    TRIGGER contiene ora tutti gli states e le  animations

--- src/app/components/open-close/open-close.component.ts
+++ src/app/components/open-close/open-close.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,26 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './open-close.component.html',
   styleUrls: ['./open-close.component.scss'],
   animations: [
-    state('open', style({
-      height: '200px',
-      opacity: 1,
-      backgroundColor: 'yellow'
-    })
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })
+      ),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
+    ],
     ),
-    state('closed', style({
-      height: '100px',
-      opacity: 0.8,
-      backgroundColor: 'blue'
-    })
-    ),
-    transition('open => closed',[
-      animate('1s')
-    ]),
-    transition('closed => open', [
-      animate('0.5s')
-    ]),
-  ]
+  ],
 })
-export class OpenCloseComponent {
-
-}
+export class OpenCloseComponent {}
