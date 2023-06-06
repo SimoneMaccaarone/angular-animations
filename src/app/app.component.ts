dd1b300bbd16726e5b9117870712c8d3f937d4fa
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-06 12:14:16 +0200

    add button (doesnt works) and animation in app.components.ts
    add button (doesnt works) and animation in app.components.ts
    
     animations: [
        trigger('showHide', [
          state('show', style({
            position: 'absolete',
            top: '500px',
            left: '500px'
          })),
          state('hide', style({
            position: 'absolete',
            top: '500px',
            left: '0'
          })),
          transition('show => hide', [animate('1s')]),
          transition('hide => show', [animate('1s')]),
        ])
      ]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,22 @@ import {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: []
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        position: 'absolete',
+        top: '500px',
+        left: '500px'
+      })),
+      state('hide', style({
+        position: 'absolete',
+        top: '500px',
+        left: '0'
+      })),
+      transition('show => hide', [animate('1s')]),
+      transition('hide => show', [animate('1s')]),
+    ])
+  ]
 })
 export class AppComponent {
   title = 'angular-animations';
