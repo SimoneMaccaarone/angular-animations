ee006e15a37917e516571ad2bed1bbbb1dc045d7
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-06 11:45:42 +0200

    aggiunta di 2 state (open, close)
    aggiunta di 2 state (open, close)

--- src/app/components/open-close/open-close.component.ts
+++ src/app/components/open-close/open-close.component.ts
@@ -1,9 +1,24 @@
+import { state, style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-open-close',
   templateUrl: './open-close.component.html',
-  styleUrls: ['./open-close.component.scss']
+  styleUrls: ['./open-close.component.scss'],
+  animations: [
+    state('open', style({
+      height: '200px',
+      opacity: 1,
+      backgroundColor: 'yellow'
+    })
+    ),
+    state('closed', style({
+      height: '100px',
+      opacity: 0.8,
+      backgroundColor: 'blue'
+    })
+    ),
+  ]
 })
 export class OpenCloseComponent {
 
